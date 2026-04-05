c91658b8bc1dabd2d6917731a358f7f4f6375229
Panayotis Vryonis <58812+vrypan@users.noreply.github.com>
2026-04-05 13:18:31 +0000

    Brew formula update for stash version v0.5.1

--- Formula/stash.rb
+++ Formula/stash.rb
@@ -1,32 +1,45 @@
 class Stash < Formula
   desc "A local store for pipeline output and ad hoc file snapshots."
   homepage "https://github.com/vrypan/stash"
-  version "0.4.4"
+  version "0.5.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/vrypan/stash/releases/download/v0.4.4/stash_0.4.4_darwin_arm64.tar.gz"
-      sha256 "24571ffb374f212601418c34a24f21920507251b2c09a0972506486a6742376d"
+      url "https://github.com/vrypan/stash/releases/download/v0.5.1/stash-aarch64-apple-darwin.tar.xz"
+      sha256 "4f1941b0b2e39b039d5fe1117f3b8be420633a3264ef4994f1f1ae5a1d7337de"
     end
     on_intel do
-      url "https://github.com/vrypan/stash/releases/download/v0.4.4/stash_0.4.4_darwin_x86_64.tar.gz"
-      sha256 "2258b80c1668bb1821329c5890daa0386289a199b86bd87054058fa5a6f36dd1"
+      url "https://github.com/vrypan/stash/releases/download/v0.5.1/stash-x86_64-apple-darwin.tar.xz"
+      sha256 "aaeadfe74bd261e27fe224068bba09cdbc76771c493fd0f01fb7ec96a476763b"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/vrypan/stash/releases/download/v0.4.4/stash_0.4.4_linux_arm64.tar.gz"
-      sha256 "75c050272a8b6db5404d3815767c6578dd24ea22e3a93bd2112bb68dee607b9a"
+      url "https://github.com/vrypan/stash/releases/download/v0.5.1/stash-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "bad24208b2e12ad0e05fb958edc61b4c60473e8fcb103f5702a54d0f9a7c4dbf"
     end
     on_intel do
-      url "https://github.com/vrypan/stash/releases/download/v0.4.4/stash_0.4.4_linux_x86_64.tar.gz"
-      sha256 "9d15387d53729e2339509d905176b0973946e2ab98e45ec7d37e68c27962b3f2"
+      url "https://github.com/vrypan/stash/releases/download/v0.5.1/stash-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "9d1615db89b18f620fac2332369eaa704a97d080280b3bdda56d1cb2560591a4"
     end
   end
 
   def install
+    cellar = HOMEBREW_CELLAR/"stash"
+    if cellar.exist?
+      installed = cellar.subdirs.map { |d| Version.new(d.basename.to_s) }.max
+      if installed && installed < Version.new("0.5.0")
+        odie <<~EOS
+          stash #{installed} is installed.
+          Automatic upgrade from versions older than 0.5.0 is not supported.
+          Visit https://github.com/vrypan/stash for more info.
+          To force the upgrade:  first, then install again.
+        EOS
+      end
+    end
+
     bin.install "stash"
     pkgshare.install "scripts" if Dir.exist?("scripts")
     (bash_completion/"stash").write Utils.safe_popen_read(
