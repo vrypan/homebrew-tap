fe54833e23994c6d4263befa015563cf89312ee6
Panayotis Vryonis <58812+vrypan@users.noreply.github.com>
2026-04-14 05:08:22 +0000

    Brew formula update for stash version v0.8.0

--- Formula/stash.rb
+++ Formula/stash.rb
@@ -1,28 +1,28 @@
 class Stash < Formula
   desc "A local store for pipeline output and ad hoc file snapshots."
   homepage "https://github.com/vrypan/stash"
-  version "0.7.1"
+  version "0.8.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/vrypan/stash/releases/download/v0.7.1/stash-cli-aarch64-apple-darwin.tar.xz"
-      sha256 "08fae7e6226a507d27bd743e8bd30f553a946a2dc5be32b0d885ca472d37ba81"
+      url "https://github.com/vrypan/stash/releases/download/v0.8.0/stash-cli-aarch64-apple-darwin.tar.xz"
+      sha256 "10dea6654877bda3a3a0ba987381d55bb45a8b11ce0b498d3bbe89c08af4c572"
     end
     on_intel do
-      url "https://github.com/vrypan/stash/releases/download/v0.7.1/stash-cli-x86_64-apple-darwin.tar.xz"
-      sha256 "29cb6f4128b6084321ef95e4844c4cbda98525b75ec5e631d515b78952cbf2a4"
+      url "https://github.com/vrypan/stash/releases/download/v0.8.0/stash-cli-x86_64-apple-darwin.tar.xz"
+      sha256 "b0577e76829fa9555e1bb4c8fa8f7a7c529faa383fb5a24d2d5bb22a3960c296"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/vrypan/stash/releases/download/v0.7.1/stash-cli-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "564101d676f51fd5b33d994b6f9af16a43e74326f4949ae98ff2bb7d6b6b3766"
+      url "https://github.com/vrypan/stash/releases/download/v0.8.0/stash-cli-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "dd1e0b85388a08d2f31d7b853e0aa6ed00bb619184a4e48ab66d6c19ded920d7"
     end
     on_intel do
-      url "https://github.com/vrypan/stash/releases/download/v0.7.1/stash-cli-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "321d96fee246fd72777c41f82887d3a85ff0895874165ef3c261c242ac0e68dd"
+      url "https://github.com/vrypan/stash/releases/download/v0.8.0/stash-cli-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "9e0bab589e5a1ee40b8352073f65704bb6f47a65769ef5f4be952f2f874b64ff"
     end
   end
 
