cd4e78bc9845fa8cc17ce7539e6d7ad4409dcecc
Panagiotis Vryonis <58812+vrypan@users.noreply.github.com>
2026-05-08 17:23:35 +0000

    Brew formula update for shg version v0.1.2

--- Formula/shg.rb
+++ Formula/shg.rb
@@ -1,9 +1,9 @@
 class Shg < Formula
   desc "Scan shell history files for accidentally persisted secrets."
   homepage "https://github.com/vrypan/shg"
-  url "https://github.com/vrypan/shg/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "f3d59fd8ceac3e15006dd6f0d72d2e884daf39737437b7393c94884533e29a45"
-  version "0.1.1"
+  url "https://github.com/vrypan/shg/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "75bb5c0ee5d8a09516332abd3508f9ac8764ec7828dac162906986d1762a23c4"
+  version "0.1.2"
   license "MIT"
 
   depends_on "zig" => :build
