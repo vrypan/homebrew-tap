41e5217a0d39e1a5ad06c49aa26f02afb55e8a90
Panagiotis Vryonis <58812+vrypan@users.noreply.github.com>
2026-05-09 05:26:06 +0000

    Brew formula update for shg version v0.1.4

--- Formula/shg.rb
+++ Formula/shg.rb
@@ -1,15 +1,19 @@
 class Shg < Formula
   desc "Scan shell history files for accidentally persisted secrets."
   homepage "https://github.com/vrypan/shg"
-  url "https://github.com/vrypan/shg/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "4763e206f60796d3add5bfc69afb67b8e84e2cc4fbb4dd16bd9f6b8ee949d125"
-  version "0.1.3"
+  url "https://github.com/vrypan/shg/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "ab973d350f6088d1a5f3652e300b5278f9d304092778734f31e4af9564ad127a"
+  version "0.1.4"
   license "MIT"
 
   depends_on "zig" => :build
 
   def install
     system "zig", "build", "--prefix", prefix
+    (share/"shg/extras").install Dir["extras/*"]
+    (share/"shg/defaults").install "src/defaults/ignore.default.shg",
+                                   "src/defaults/match.default.shg",
+                                   "src/defaults/paths.default.shg"
   end
 
   test do
