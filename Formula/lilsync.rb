4de26b4e6cf7c5accf369facd3521a53b81522ef
Panagiotis Vryonis <58812+vrypan@users.noreply.github.com>
2026-05-17 18:54:26 +0000

    Brew formula update for lilsync version v0.3.2

--- Formula/lilsync.rb
+++ Formula/lilsync.rb
@@ -1,16 +1,16 @@
 class Lilsync < Formula
   desc "Peer-to-peer folder sync for small trusted groups on a LAN."
   homepage "https://github.com/vrypan/lilsync"
-  url "https://github.com/vrypan/lilsync/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "64ee8b756e68b350e83fa0c40eb75c6ff139e573bc9ff855baade7abcca71bf1"
-  version "0.3.1"
+  url "https://github.com/vrypan/lilsync/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "cd84259c70158bbbee6d07fa25a46c5a9209d69e88c8def6c8daea1958d7b1c9"
+  version "0.3.2"
   license "MIT"
   head "https://github.com/vrypan/lilsync.git", branch: "main"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args
+    system "cargo", "install", "--profile", "dist", *std_cargo_args
   end
 
   test do
