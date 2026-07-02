6338fd19aa99478e96e661164ef0879475f724ca
Panagiotis Vryonis <58812+vrypan@users.noreply.github.com>
2026-07-02 05:52:51 +0000

    Brew formula update for lilsync version v0.3.4

--- Formula/lilsync.rb
+++ Formula/lilsync.rb
@@ -1,9 +1,9 @@
 class Lilsync < Formula
   desc "Peer-to-peer folder sync for small trusted groups on a LAN."
   homepage "https://github.com/vrypan/lilsync"
-  url "https://github.com/vrypan/lilsync/archive/refs/tags/v0.3.3.tar.gz"
-  sha256 "2442763b616da9bcb7370cc63939acb374ac713896dd4e4288f0f03887aa3e39"
-  version "0.3.3"
+  url "https://github.com/vrypan/lilsync/archive/refs/tags/v0.3.4.tar.gz"
+  sha256 "cb61496176efbc55828a16e747b557eb8c4fb060a489f20353089169d69c5be5"
+  version "0.3.4"
   license "MIT"
   head "https://github.com/vrypan/lilsync.git", branch: "main"
 
