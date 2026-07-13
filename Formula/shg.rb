33d310001dc0a35cf658eb8ad5612f467d4d0ae7
Panagiotis Vryonis <58812+vrypan@users.noreply.github.com>
2026-07-13 11:05:31 +0000

    Brew formula update for shg version v0.2.6

--- Formula/shg.rb
+++ Formula/shg.rb
@@ -1,9 +1,9 @@
 class Shg < Formula
   desc "Scan histories, environment, and AI agent transcripts for secrets."
   homepage "https://github.com/vrypan/shg"
-  url "https://github.com/vrypan/shg/archive/refs/tags/v0.2.5.tar.gz"
-  sha256 "a165cbab245a148f69d7e8638e2aa5b705b86ee9664869ca70ba111fce2f9c48"
-  version "0.2.5"
+  url "https://github.com/vrypan/shg/archive/refs/tags/v0.2.6.tar.gz"
+  sha256 "38cfff5490e7ab2c654df31f68074edb15c7d3cb27f694834f549805f3f3b88b"
+  version "0.2.6"
   license "MIT"
 
   depends_on "zig" => :build
