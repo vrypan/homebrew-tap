beb77d85c9e631ddc6f41085fa01d73c8d539d90
Panayotis Vryonis <58812+vrypan@users.noreply.github.com>
2026-09-25 17:42:57 +0000

    Update statusbar to v0.4.0

--- Formula/statusbar.rb
+++ Formula/statusbar.rb
@@ -1,9 +1,9 @@
 class Statusbar < Formula
   desc "Status bar for any terminal"
   homepage "https://github.com/vrypan/statusbar"
-  url "https://github.com/vrypan/statusbar/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "b2d306c89ea6f2cc19b21d0f7caed9f795494fa97f59d9b85471c7c6c86ef7b1"
-  version "0.3.1"
+  url "https://github.com/vrypan/statusbar/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "6e1f38c466fed90ed0ffe6aa637d958957f6ce8c5e1c25c4656cea44e068090c"
+  version "0.4.0"
   head "https://github.com/vrypan/statusbar.git", branch: "main"
 
   depends_on "zig@0.16" => :build
